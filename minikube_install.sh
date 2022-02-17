0370e0135aed653be2ff1cba88b51ed47a5d378a
#!/bin/bash <84236806+jang-jinyeol@users.noreply.github.com>
2022-02-17 20:21:13 +0900

    Update minikube_install.sh

--- minikube_install.sh
+++ minikube_install.sh
@@ -1,10 +1,2 @@
-sudo apt install virtualbox virtualbox-ext-pack -y
-
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-  && chmod +x minikube
-sudo mkdir -p /usr/local/bin/
-sudo install minikube /usr/local/bin/
-
-sudo apt-get install -y conntrack
-
-sudo minikube start --vm-driver=none
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
