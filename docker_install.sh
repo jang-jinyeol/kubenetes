12bc2e2242cea36ef239d7888f934969f7c52891
#!/bin/bash <84236806+jang-jinyeol@users.noreply.github.com>
2022-02-22 02:35:39 +0900

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -5,6 +5,12 @@ sudo apt-get install \
     curl \
     gnupg-agent \
     software-properties-common -y
+
+
+-----------------------
+
+
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
